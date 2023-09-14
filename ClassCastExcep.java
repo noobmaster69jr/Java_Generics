2f4f0f4548529fb3bcf2778d2a5e1284c8e8acb3
noobmaster69jr <srashwin22@gmail.com>
2023-09-14 15:12:26 +0530

    ClassCastException

---
+++ ClassCastExcep.java
@@ -0,0 +1,13 @@
+package Generics;
+
+public class ClassCastExcep {
+
+	// Generics helps us perform compile time checking and avoid ClassCastException
+	public static void main(String[] args) {
+		// Comparable number = 10;
+		// number.compareTo("10");
+		Comparable<Integer> number = 10;
+		number.compareTo(10);
+	}
+
+}
